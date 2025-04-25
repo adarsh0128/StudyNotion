fdd82de65fd70bf956b0548391ac33c8f23eb23e
Adarsh kumar <adarshkumar0128@gmail.com>
2025-04-25 16:48:50 +0530

    Controllers completed

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,10 @@ const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
   mongoose
-    .connect(MONGODB_URL)
+    .connect(MONGODB_URL, {
+      useNewUrlparser: true,
+      useUnifiedTopology: true,
+    })
     .then(console.log(`DB Connection Success`))
     .catch((err) => {
       console.log(`DB Connection Failed`);
